4bec0f1d6a323684f277ddb945a34cd000bd7565
gulyasfeccferenc <gulyasfeccferenc@gmail.com>
2016-01-02 13:05:40 +0100

    Set up mongodb connection

--- index.js
+++ index.js
@@ -5,11 +5,40 @@
 * 
 */
 var http = require('http'),
-	router = require('./router');
+	mongodb = require("mongodb"),
+	router = require('./router'),
+	MongoClient = require('mongodb').MongoClient,
+	assert = require('assert'),
+	uri = "mongodb://admin:admin@ds037415.mongolab.com:37415/oac__word",
+	wordList = new Array();
 
-http.createServer(function (request, response) {
-  router.home(request, response);
-  router.generate(request, response);
-  router.serve(request, response);
-}).listen(process.env.PORT || 80);
+function mongoAccess(callback) {
+	MongoClient.connect(uri, function(err, db) {
+		var words = db.collection('words');
+		if(err) throw err;
+
+		words.find().toArray(function (err, docs) {
+			if (err) throw err;
+
+			docs.forEach(function(doc) {
+				wordList.act += doc['act'];
+				wordList.draw += doc['draw'];
+				wordList.describe += doc['describe'];
+			})
+
+		console.log('Needed data fetched');
+		});
+	});
+	callback();
+}
+
+function serverInit() {
+	http.createServer(function (request, response) {
+		router.home(request, response);
+		router.generate(request, response);
+		router.serve(request, response);
+	}).listen(process.env.PORT || 80);
+}
+
+mongoAccess(serverInit);
 console.log('Server initialized at port ' + (process.env.PORT || 80));
